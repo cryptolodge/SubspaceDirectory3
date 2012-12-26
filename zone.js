e9e9f3e2c0f161b3df2c93ff973b95516269b6c3
docflabby <kshade2001@yahoo.co.uk>
2012-12-26 01:34:06 +0000

    a few more changes

--- zone.js
+++ zone.js
@@ -1,3 +1,17 @@
 /**
  * New node file
  */
+
+// Constructor
+var Zone = function() {
+  
+}
+// properties and methods
+Zone.prototype = {
+  ip: "default_value",
+  method: function(argument) {
+    this.value2 = argument + 100;
+  }
+};
+// node.js module export
+module.exports = Zone;
