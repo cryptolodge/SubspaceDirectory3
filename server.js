f899d41f3125b8cda149ebb573075e26b0fd2631
docflabby <kshade2001@yahoo.co.uk>
2012-12-26 01:28:07 +0000

    initial noide files

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * New node file
+ */
+ 
+var dgram = require("dgram");
+
+var server = dgram.createSocket("udp4");
+
+server.on("message", function (msg, rinfo) {
+	console.log(msg.toString("hex"));	
+  console.log("server got: " + msg + " from " +
+    rinfo.address + ":" + rinfo.port);
+});
+
+server.on("listening", function () {
+  var address = server.address();
+  console.log("server listening " +	
+      address.address + ":" + address.port);
+});
+
+server.bind(4990);
